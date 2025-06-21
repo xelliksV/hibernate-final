2811eaa98e180491a2aceaab0de97e2e4a768055
Vladislav <xelliks666@gmail.com>
2025-06-21 18:20:53 +0700

    Added DAO classes

--- src/main/java/com/javarush/dao/CountryDAO.java
+++ src/main/java/com/javarush/dao/CountryDAO.java
@@ -1,4 +1,18 @@
 package com.javarush.dao;
 
+import com.javarush.domain.Country;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class CountryDAO {
+    private final SessionFactory sessionFactory;
+    public CountryDAO(SessionFactory factory) {
+        this.sessionFactory = factory;
+    }
+    public List<Country> getAll() {
+        Query<Country> fromCountry = sessionFactory.getCurrentSession().createQuery("from Country", Country.class);
+        return fromCountry.list();
+    }
 }
